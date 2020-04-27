2fee4fcbb1e932baefe4772fc56b08c7542be976
Alvaro Navarro <alvaro.navarro@amadeus.com>
2020-04-27 16:25:55 +0200

    Add Flight Create Orders API

---
+++ Sources/Amadeus/Booking/FlightOrders.swift
@@ -0,0 +1,23 @@
+import Foundation
+import SwiftyJSON
+
+public class FlightOrders {
+    private var client: Client
+
+    public init(client: Client) {
+        self.client = client
+    }
+
+    /// Books a flight
+    ///
+    /// - Returns:
+    ///    `JSON` object
+    ///
+   public func post(body: JSON, params: [String: String] = [:], onCompletion: @escaping AmadeusResponse) {
+
+       client.post(path: "/v1/booking/flight-orders", body: body, params: params, onCompletion: {
+                       response, error in
+                       onCompletion(response, error)
+                   })
+   }
+}
