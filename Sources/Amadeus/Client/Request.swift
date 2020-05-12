cc739c6ec23783099d95e574b7c2ad0084c4a11a
James Dale <git@jamesdale.com.au>
2020-05-12 23:23:59 +1000

    Update Client

--- Sources/Amadeus/Client/Request.swift
+++ Sources/Amadeus/Client/Request.swift
@@ -1,53 +1,58 @@
 import Foundation
 
-public typealias AmadeusResponse = (Response?, ResponseError?) -> Void
+public typealias AmadeusResponse = (Result<Response, ResponseError>) -> Void
 
 public func send(verb: String,
-                    url: String,
-                    headers: [String: String],
-                    body: String?,
-                    onCompletion: @escaping AmadeusResponse) {
-
+                 url: String,
+                 headers: [String: String],
+                 body: String?,
+                 onCompletion: @escaping AmadeusResponse) {
+    
     if let url = URL(string: url) {
         let request = NSMutableURLRequest(url: url)
-
+        
         request.httpMethod = verb
-
+        
         if body != nil {
             request.httpBody = body!.data(using: String.Encoding.utf8)
         }
-
+        
         for header in headers {
             request.setValue(header.value, forHTTPHeaderField: header.key)
         }
-
-        process(request: request, onCompletion: { (response, error) -> Void in
-            onCompletion(response, error)
-             })
+        
+        process(request: request, onCompletion: { result -> Void in
+            onCompletion(result)
+        })
     } else {
-        onCompletion(nil, ResponseError.malformedURL)
+        onCompletion(.failure(.malformedURL))
     }
 }
 
 private func process(request: NSMutableURLRequest,
-                  onCompletion: @escaping AmadeusResponse) {
+                     onCompletion: @escaping AmadeusResponse) {
     let session = URLSession.shared
     let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
-
+        
         var amadeusResponse: Response?
         var amadeusError: ResponseError?
-
-        if let httpResponse = response as? HTTPURLResponse {
-            // got a valid HTTP answer
-            amadeusResponse = Response(response: httpResponse, data: data!)
-            // Error could be either nil (200 OK) or enum value
-            amadeusError = amadeusResponse!.getErrorCode()
-        } else {
+        
+        guard let httpResponse = response as? HTTPURLResponse else {
             // no HTTP answer: network problem
-            amadeusResponse = nil
-            amadeusError = ResponseError.returnedError(error!)
+            onCompletion(.failure(.returnedError(error!)))
+            return
         }
-        onCompletion(amadeusResponse, amadeusError)
+        
+        let response = Response(response: httpResponse, data: data!)
+        
+        // Error could be either nil (200 OK) or enum value
+        if let error = response.getErrorCode() {
+            onCompletion(.failure(error))
+        } else {
+            // got a valid HTTP answer
+            onCompletion(.success(response))
+        }
+        
     })
     task.resume()
 }
