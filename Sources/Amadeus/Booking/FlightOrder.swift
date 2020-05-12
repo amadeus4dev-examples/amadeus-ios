536cc4a9f731590217c1c18441900b2c59ed45c8
James Dale <git@jamesdale.com.au>
2020-05-12 23:23:52 +1000

    Update Booking

--- Sources/Amadeus/Booking/FlightOrder.swift
+++ Sources/Amadeus/Booking/FlightOrder.swift
@@ -3,25 +3,25 @@ import Foundation
 public class FlightOrder {
     private var client: Client
     private var flightOrderId: String
-
+    
     public init(client: Client, flightOrderId: String) {
         self.client = client
         self.flightOrderId = flightOrderId
     }
-
+    
     /// Retrieves a flight order based on its ID.
     public func get(onCompletion: @escaping AmadeusResponse) {
         client.get(path: "v1/booking/flight-orders/\(self.flightOrderId)", params: [:], onCompletion: {
-                       response, error in
-                       onCompletion(response, error)
-                   })
+            result in
+            onCompletion(result)
+        })
     }
-
+    
     /// Deletes a flight order based on its ID.
     public func delete(onCompletion: @escaping AmadeusResponse) {
         client.delete(path: "v1/booking/flight-orders/\(self.flightOrderId)", params: [:], onCompletion: {
-                       response, error in
-                       onCompletion(response, error)
-                   })
+            result in
+            onCompletion(result)
+        })
     }
 }
